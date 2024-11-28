da7e781f13fa572be4811880da56dd245410fd4a
Jackson Lewis <hello@jacksonlewis.co.uk>
2024-11-28 20:10:26 +0000

    formatting fix

--- frontend/src/pages/layout.tsx
+++ frontend/src/pages/layout.tsx
@@ -3,42 +3,42 @@ import { isUserLoggedIn } from '@/lib/user'
 import { Link, Outlet } from 'react-router-dom'
 
 export default function Layout() {
-    return (
-        <CategoriesProvider>
-            <header>
-                <Link to="/">Finance Tracker</Link>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/expenses">Expenses</Link>
-                            <ul>
-                                <li>
-                                    <Link to="/expenses/recurring">Recurring</Link>
-                                </li>
-                                <li>
-                                    <Link to="/expenses/categories">Categories</Link>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <Link to="/income">Income</Link>
-                        </li>
-                        <li>
-                            <Link to="/reports">Reports</Link>
-                        </li>
-                        {isUserLoggedIn() ? (
-                          <li>
-                            <Link to="/account">Account</Link>
-                          </li>
-                        ) : (
-                          <li>
-                            <Link to="/login">Login</Link>
-                          </li>
-                        )}
-                    </ul>
-                </nav>
-            </header>
-            <Outlet />
-        </CategoriesProvider>
-    )
+  return (
+    <CategoriesProvider>
+      <header>
+        <Link to="/">Finance Tracker</Link>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/expenses">Expenses</Link>
+              <ul>
+                <li>
+                  <Link to="/expenses/recurring">Recurring</Link>
+                </li>
+                <li>
+                  <Link to="/expenses/categories">Categories</Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to="/income">Income</Link>
+            </li>
+            <li>
+              <Link to="/reports">Reports</Link>
+            </li>
+            {isUserLoggedIn() ? (
+              <li>
+                <Link to="/account">Account</Link>
+              </li>
+            ) : (
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </header>
+      <Outlet />
+    </CategoriesProvider>
+  );
 }
