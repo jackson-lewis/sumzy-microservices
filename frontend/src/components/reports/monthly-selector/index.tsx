48da75a58cc58495b4cad3706fa3ce365d2a6181
Jackson Lewis <hello@jacksonlewis.co.uk>
2024-11-28 19:45:09 +0000

    added monthly selector UI on reports

---
+++ frontend/src/components/reports/monthly-selector/index.tsx
@@ -0,0 +1,52 @@
+import { ChangeEvent, Dispatch, SetStateAction } from 'react'
+
+export default function MonthlySelector({
+  activeYM,
+  setActiveYM
+}: {
+  activeYM: [number, number]
+  setActiveYM: Dispatch<SetStateAction<[number, number]>>
+}) {
+  function handleChange(event: ChangeEvent<HTMLSelectElement>) {
+    const { value } = event.target
+    const valueAsNumber = Number(value)
+
+    setActiveYM((ym) => {
+      if (value.length === 4) {
+        return [
+          valueAsNumber,
+          ym[1]
+        ]
+      } else if (valueAsNumber > 0 && valueAsNumber <= 12) {
+        return [
+          ym[0],
+          valueAsNumber
+        ]
+      }
+      return ym
+    })
+  }
+
+  return (
+    <div>
+      <select
+        name="year"
+        id="year"
+        value={activeYM[0]}
+        onChange={handleChange}
+      >
+        <option>2024</option>
+      </select>
+      <select
+        name="month"
+        id="month"
+        value={activeYM[1]}
+        onChange={handleChange}
+      >
+        <option>9</option>
+        <option>10</option>
+        <option>11</option>
+      </select>
+    </div>
+  )
+}
--- frontend/src/pages/reports/index.tsx
+++ frontend/src/pages/reports/index.tsx
@@ -1,5 +1,6 @@
 import Money from '@/components/global/money'
 import Category from '@/components/reports/category'
+import MonthlySelector from '@/components/reports/monthly-selector'
 import { getExpenseReports } from '@/lib/reports'
 import { Report } from '@/types'
 import { useEffect, useState } from 'react'
@@ -7,9 +8,15 @@ import { useEffect, useState } from 'react'
 export default function Reports() {
   const [report, setReport] = useState<Report>()
 
+  const date = new Date()
+  const [activeYM, setActiveYM] = useState<[number, number]>([
+    date.getFullYear(),
+    date.getMonth() + 1
+  ])
+
   useEffect(() => {
     async function getData() {
-      const data = await getExpenseReports(2024, 11)
+      const data = await getExpenseReports(activeYM[0], activeYM[1])
 
       if (data instanceof Error) {
         alert(data.message)
@@ -19,11 +26,15 @@ export default function Reports() {
       setReport(data)
     }
     getData()
-  }, [])
+  }, [activeYM])
 
   return (
     <main>
       <h1>Monthly Reports</h1>
+      <MonthlySelector
+        activeYM={activeYM}
+        setActiveYM={setActiveYM}
+      />
       {report ? (
         <>
           <Money amount={report.total} />
