4fcb97ac44b1ac25ed8035041f2988968749e3ff
Jackson Lewis <hello@jacksonlewis.co.uk>
2024-12-07 18:27:26 +0000

    updated UI with site header and mobile nav bar

--- frontend/src/pages/layout.tsx
+++ frontend/src/pages/layout.tsx
@@ -1,43 +1,13 @@
+import DashboardHeader from '@/components/layout/header'
+import MobileNavBar from '@/components/layout/mobile-nav-bar'
 import CategoriesProvider from '@/lib/expense-context'
-import { isUserLoggedIn } from '@/lib/user'
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
   return (
     <CategoriesProvider>
-      <header>
-        <Link to="/">Finance Tracker</Link>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/expenses">Expenses</Link>
-              <ul>
-                <li>
-                  <Link to="/expenses/recurring">Recurring</Link>
-                </li>
-                <li>
-                  <Link to="/expenses/categories">Categories</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link to="/income">Income</Link>
-            </li>
-            <li>
-              <Link to="/reports">Reports</Link>
-            </li>
-            {isUserLoggedIn() ? (
-              <li>
-                <Link to="/account">Account</Link>
-              </li>
-            ) : (
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-            )}
-          </ul>
-        </nav>
-      </header>
+      <DashboardHeader />
+      <MobileNavBar />
       <Outlet />
     </CategoriesProvider>
   );
