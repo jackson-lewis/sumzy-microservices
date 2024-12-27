c46c3b593dc9a0f5af8e524018e66f4e416114e1
Jackson Lewis <hello@jacksonlewis.co.uk>
2024-12-27 18:47:42 +0000

    added tx functions

--- frontend/src/lib/transactions.ts
+++ frontend/src/lib/transactions.ts
@@ -1,4 +1,4 @@
-import { Transaction } from '@/types'
+import { Category, Transaction, TransactionDirection } from '@/types'
 import { apiRequest } from './api'
 
 /**
@@ -26,4 +26,52 @@ export async function createTransaction(
     transaction,
     true
   )
+}
+
+
+/**
+ * Update a transaction for the authenticated user.
+ */
+export async function updateTransaction(
+  transaction: Transaction
+): Promise<Transaction | Error> {
+  return await apiRequest(
+    'v1/transactions',
+    'PATCH',
+    transaction,
+    true
+  )
+}
+
+
+/**
+ * Delete a transaction for the authenticated user.
+ */
+export async function deleteTransaction(
+  id: Transaction['id']
+): Promise<Transaction | Error> {
+  return await apiRequest(
+    `v1/transactions?id=${id}`,
+    'DELETE',
+    null,
+    true
+  )
+}
+
+export function txDirection(
+  transaction: Transaction
+): TransactionDirection {
+  return transaction.amount < 0 ? 'expense' : 'income'
+}
+
+/**
+ * Get the category for the transaction
+ */
+export function getTransactionCategory(
+  transaction: Transaction,
+  categories: Category[]
+) {
+  return categories.find((category) => {
+    return category.id === transaction.category
+  })
 }
