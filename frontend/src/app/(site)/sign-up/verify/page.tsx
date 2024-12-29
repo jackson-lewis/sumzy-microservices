c33453ec26ce8087c292cc6fa65bb6599d0192f7
Jackson Lewis <hello@jacksonlewis.co.uk>
2024-12-29 22:54:54 +0000

    wrapped in suspense

--- frontend/src/app/(site)/sign-up/verify/page.tsx
+++ frontend/src/app/(site)/sign-up/verify/page.tsx
@@ -1,6 +1,7 @@
 import VerifyMessage from '@/components/site/verify'
 import styles from './page.module.scss'
 import { Metadata } from 'next'
+import { Suspense } from 'react'
 
 export const metadata: Metadata = {
   title: 'Verify your email ~ Sumzy',
@@ -10,7 +11,9 @@ export const metadata: Metadata = {
 export default function SignUpVerify() {
   return (
     <main className={styles.main}>
-      <VerifyMessage />
+      <Suspense>
+        <VerifyMessage />
+      </Suspense>
     </main>
   )
 }
