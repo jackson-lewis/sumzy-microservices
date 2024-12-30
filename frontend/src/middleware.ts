9a4c3e032039bf1e389fa7861bc6abd0fa1bd2d8
Jackson Lewis <hello@jacksonlewis.co.uk>
2024-12-30 16:18:18 +0000

    added comments for redirects

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -6,18 +6,27 @@ import type { NextRequest } from 'next/server'
 export async function middleware(request: NextRequest) {
   const cookieStore = await cookies()
 
-  if (request.nextUrl.pathname.startsWith('/login')) {
+  /**
+   * Redirect to dashboard if user is already logged in
+   */
+  if (request.nextUrl.pathname.startsWith('/sign-in')) {
     if (cookieStore.get('token')) {
       return NextResponse.redirect(new URL('/dashboard', request.url))
     }
   }
 
+  /**
+   * Redirect to sign in if user is not signed in
+   */
   if (request.nextUrl.pathname.startsWith('/dashboard')) {
     if (!cookieStore.get('token')) {
-      return NextResponse.redirect(new URL('/login', request.url))
+      return NextResponse.redirect(new URL('/sign-in', request.url))
     }
   }
 
+  /**
+   * Redirect to reports page with current year and month
+   */
   if (request.nextUrl.pathname.startsWith('/dashboard/reports')) {
     const year = request.nextUrl.searchParams.get('year')
     const month = request.nextUrl.searchParams.get('month')
