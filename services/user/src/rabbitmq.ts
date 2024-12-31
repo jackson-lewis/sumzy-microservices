a3e7fccf176b28962137dc496ec23bff7cc2e30a
Jackson Lewis <hello@jacksonlewis.co.uk>
2024-12-31 17:40:30 +0000

    setup for aws

--- services/user/src/rabbitmq.ts
+++ services/user/src/rabbitmq.ts
@@ -1,7 +1,7 @@
 import amqp from 'amqplib'
 import { User } from '@prisma/client'
 
-export const RABBITMQ_URL = 'amqp://rabbitmq'
+export const RABBITMQ_URL = process.env.AMQP_URL
 export const QUEUE_NAME = 'user-sign-up'
 export let channel: amqp.Channel
 
